da1d3d377d7f48027ab29f8649428d191c9bbe3e
n.abbasi <nimaab1992@gmail.com>
2022-02-14 09:14:07 +0330

    firs commit

---
+++ dist/main.js
@@ -0,0 +1,1 @@
+(()=>{var r={548:r=>{r.exports=function(r,o){return r+o}}},o={};function t(e){var n=o[e];if(void 0!==n)return n.exports;var s=o[e]={exports:{}};return r[e](s,s.exports,t),s.exports}(()=>{const r=t(548)(10,11);console.log(r)})()})();
---
+++ src/index.js
@@ -0,0 +1,4 @@
+const sum = require("./sum");
+
+const x = sum(10, 11);
+console.log(x);
